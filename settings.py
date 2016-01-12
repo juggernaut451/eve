bc6af0b78427f307360ba5a524e8461eb29c8b98
ashu94 <singhjug1994@gmail.com>
2016-01-13 03:21:37 +0530

    auth inialise

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-from app import Authenticate
+from auth import Authenticate
 MONGO_DBNAME = "eve"
 #AUTH_FIELD = 'username'
 RESOUCE_METHODS = ['GET']
@@ -31,7 +31,7 @@ DOMAIN = {
 
     },
     'login' : {
-    #'authentication': Authenticate,
+    'authentication': Authenticate,
     'datasource': {
         'source': 'signup',
         },
